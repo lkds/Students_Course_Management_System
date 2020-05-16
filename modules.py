9a558f1b5a4f23d419cc0f5a7c4c38c38c136bdf
hanze5 <1479320516@qq.com>
2020-05-16 19:29:11 +0800

    大泽的版本1

--- modules.py
+++ modules.py
@@ -2,8 +2,61 @@ from DB import DB
 
 db = DB()
 
-
+###################查询课程#################
 def getCourse():
-    sql = """select * from student"""
+    """
+    获取所有课程
+    """
+    sql = """select * from course"""
     db.cur.execute(sql)
-    return db.cur.fetchone()
+    return db.cur.fetchall()
+
+def getCoursefromId(cId):
+    """
+    根据id查询课程
+    """
+    sql = """select * from course where cId = '{}'""" .format(cId)
+    db.cur.execute(sql)
+    return db.cur.fetchall()
+
+def getCoursefromName(cName):
+    """
+    根据课程名字模糊查询课程
+    """
+    sql = """select * from course where  cName like '%{}%'""" .format(cName)
+    db.cur.execute(sql)
+    return db.cur.fetchall()
+
+
+###########################################
+#添加课程
+def addNewCourse(cName,cDepart,cTeacher):
+    """
+    添加课程
+    """
+    sql = 'insert into course(cName,cDepart,cTeacher) values({},{},{})'.format(cName,cDepart,cTeacher)
+    db.cur.execute(sql)
+
+
+
+###########删除课程##########
+def delCoursefromID(cId):
+    """
+    根据课程id删除课程
+    """
+    sql="""DELETE FROM course WHERE  cId = '{}'""" .format(cId)
+
+def delCoursefromName(cName):
+    """
+    根据课程名称删除课程
+    """
+    sql="""DELETE FROM course WHERE  cName like '%{}%'""" .format(cName)
+
+
+###############修改课程############
+def modfCourse(cId,cName,cDepart,cTeacher):
+    """
+    修改对应id的课程信息
+    """
+    sql="""UPDATE course SET cName='{}',cDepart='{}',cTeacher='{}' WHERE  cId = '{}' """.format(cName,cDepart,cTeacher,cId)
+
